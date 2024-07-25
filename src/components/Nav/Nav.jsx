8d829b5b409b22e6e7e8f9094de5adfbf36e8168
Josephmusngi21 <josephmusngi7@gmail.com>
2024-07-25 15:39:48 -0700

    updated nav

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '/Nav.module.css';
+
+function Nav() {
+  return (
+    <div>
+        <Nav>
+
+        </Nav>
+    </div>
+  )
+}
+
+export default Nav
