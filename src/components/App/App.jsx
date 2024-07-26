353f87ba200dbcd088f60d451e5c7633115ca6a0
Luckie Musngi <luckie.musngi@gmail.com>
2024-07-25 18:44:03 -0700

    updated card

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from 'react';
+import Main from '../Main/Main';
 
 function App() {
   return (
-    <div>App</div>
+    <div><Main/></div>
   )
 }
 
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React from 'react';
+import style from './Card.module.css';
 
 function Card() {
   return (
-    <div>Card</div>
+    <div className={style.background}>
+        <div className={style.center}>
+            <p className={style.color}>
+                Hello, my name is
+            </p>
+            <h1 className={style.color}>
+                Luckie Musngi. <br/>
+                Programmer and stuff.
+            </h1>
+            <p className={style.color}>
+                Big paragraph of
+                stuff a-nd mroe 
+                stuff
+            </p>
+
+            <div>
+                <button className={style.button}>
+                    Contact
+                </button>
+            </div>
+        </div> 
+    </div>
   )
 }
 
